a100cdb7f95bb9d49668344ac52a7a49fc58982f
Elias Lorandi <lorandi.elias@gmail.com>
2025-11-12 09:46:31 -0300

    att

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', function(){
+    echo "<h1>Welcome to CodeIgniter 4!</h1>";
+});
