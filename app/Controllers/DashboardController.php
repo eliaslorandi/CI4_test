65ca2fa1c0c7e8b0629009fa2b86bb48e77f515f
Elias Lorandi <lorandi.elias@gmail.com>
2025-11-20 09:46:47 -0300

    melhorar o dashboardcontroller

---
+++ app/Controllers/DashboardController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\TaskModel;
+
+class DashboardController extends Controller
+{
+    protected $helpers = ['url', 'form', 'session'];
+
+    protected $taskModel;
+
+    public function __construct()
+    {
+        $this->taskModel = model(TaskModel::class);
+    }
+
+    public function index()
+    {
+
+        $userId = session()->get('user_id');
+        
+        $totalTasks = $this->taskModel->countUserTasks($userId);
+
+        $tasksCompleted = $this->taskModel->countCompletedTasks($userId);
+
+        $tasksPending = $totalTasks - $tasksCompleted; 
+
+        $data = [
+            'title' => 'Dashboard',
+            // Variáveis de estatísticas que o View utilizará
+            'total_tasks' => $totalTasks,
+            'tasks_completed' => $tasksCompleted,
+            'tasks_pending' => $tasksPending, 
+
+        ];
+
+        return view('dashboard/index', $data);
+    }
+}
